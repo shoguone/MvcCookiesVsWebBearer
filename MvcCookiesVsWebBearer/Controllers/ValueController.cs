1d5a25a01c2d3582ded52c341b5b6cd0cacd82c2
Vladimir <Alemasov>
2016-11-12 04:12:41 +0700

    added using identity server for auth created test end-point /Value for access secured data

---
+++ MvcCookiesVsWebBearer/Controllers/ValueController.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json.Linq;
+using System.Web.Mvc;
+
+namespace MvcCookiesVsWebBearer.Controllers
+{
+    [Authorize]
+    public class ValueController : Controller
+    {
+        // GET: Value
+        public ActionResult Index()
+        {
+            var result = new string[] { "value1", "value2" };
+            var jsonArray = JArray.FromObject(result);
+            var jsonString = jsonArray.ToString();
+            return View((object)jsonString);
+        }
+    }
+}
